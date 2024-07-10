6390345f69b48c7d3b53f3c516cd0e41ffa8b1ed
StefanHadzhiev <stefanhadzhiev24@gmail.com>
2024-07-10 18:54:28 +0300

    Get localstorage movies with callback func in useState

--- src/App-v2.js
+++ src/App-v2.js
@@ -10,10 +10,15 @@ const average = (arr) =>
 export default function App2() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  //  const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
